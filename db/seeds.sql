48d0bbcf4caa9188dd8ac4715bbbb02ead6559cb
Richelle Burke <burkeri@alumni.upenn.edu>
2018-12-20 13:06:20 -0500

    db built and populated with data

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUE ("Cheese Burger");
+
+INSERT INTO burgers (burger_name)
+VALUE ("Steak Burger");
+
+INSERT INTO burgers (burger_name)
+VALUE ("Bacon Burger");
